3864df6b402711e02584279f7eb7e5d02258b0eb
contatobrian2009-code <contato.brian2009@gmail.com>
2026-03-18 12:00:56 -0300

    Add files

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+#Exercio 1.1
+print("Hello world!")
+#Exercio 1.2 
+print(" Meu nome é Python.")
+print("Eu sou uma linguagem de programação.")
+print("Fui criada em 1991.")
+#Exercio 1.3 
+print("Eu adoro \n programar") 
+#Exercicio 2.1
+Nome = input("coloque seu nome:")
+Idade = input("coloque sua idade:")
+print("seu nome é " , Nome , "você tem " , Idade , "anos")
+#Exercicio 2.2
+Cidade = input("coloque a sua cidade: ")
+País = input("Coloque seu país: ")
+População = input("Coloque o número de pessoas")
+print("sua cidade:" , Cidade , "," , País , "sua cidade tem" , População , "de pessoas")
